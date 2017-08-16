ee9721a2f5a877ca40346294f9cabf083bc5b3a0
Joshua Breeden <jbree@users.noreply.github.com>
2017-08-15 22:19:40 -0400

    Add VotingBooth class

---
+++ VotingBooth.js
@@ -0,0 +1,38 @@
+
+function VotingBooth (config) {
+    /* choices: string[] of possible survey options */
+    var question = config.question;
+    var choices = config.choices;
+
+    /* vote count for each choice */
+    var votes = {};
+
+    choices.forEach(function (choice) {
+        votes[choice] = 0;
+    });
+
+    this.getBallotInfo = function () {
+        return {
+            question: question,
+            choices: choices
+        };
+    }
+
+    // return true if vote was successfully cast, false otherwise
+    this.castVote = function (choice) {
+        console.log("castVote: " + choice);
+        if (votes.hasOwnProperty(choice)) {
+            votes[choice]++;
+            return true;
+        }
+        return false;
+    }
+
+    // return an object with the vote count for each choice
+    this.getVoteTally = function () {
+        console.log(votes);
+        return votes;
+    }
+}
+
+module.exports = VotingBooth;
